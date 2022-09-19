b6500598c6e3d49872bb15a5d1ae07e5e968fd65
SenorPepe92 <adenpetersen92@gmail.com>
2022-09-19 18:41:10 +0100

    Up to Step 4 of Rock Paper Scissors project

---
+++ rockPaperScissors/js/rockPaperScissors.js
@@ -0,0 +1,14 @@
+// console.log('This is to make sure CL is working');
+
+const rps = ['rock', 'paper', 'scissors'] 
+
+// Create a function that will randomly return rock, paper or scissors
+let getComputerChoice = () => { 
+    let randomComputerChoice = rps[Math.floor((Math.random() * rps.length))];
+    return randomComputerChoice;
+}
+
+let singleRound (playerSelection, computerSelection) => {
+    
+  
+}
